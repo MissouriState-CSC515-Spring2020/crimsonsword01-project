1e9003017f2c7cb448b23249b116c3c7a56b934b
Allison Dorsett <ANDorsett45@gmail.com>
2020-02-06 18:37:31 -0600

    add checkpoint_2

--- src/update.js
+++ src/update.js
@@ -1,10 +1,6 @@
-import format from 'date-fns/format';
-
-var span = document.querySelector('#time-now');
-var header = document.querySelector('#hello');
 
 export default function update() {
-	header.textContent = "Hello World!";
-	span.textContent = format(new Date(), 'h:mm:ssa');
+
+
 	setTimeout(update, 1000);
 }
